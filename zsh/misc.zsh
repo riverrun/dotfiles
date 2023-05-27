89e6b2bf33b306147c5c8eb162f15b4f7451f47b
David Whitlock <alovedalongthe@gmail.com>
2023-05-27 16:16:34 +0700

    small edit to zsh config

--- zsh/misc.zsh
+++ zsh/misc.zsh
@@ -1,14 +1,19 @@
-## smart urls
+# edit commands in editor
+autoload -z edit-command-line
+zle -N edit-command-line
+bindkey "^X^E" edit-command-line
+
+# smart urls
 autoload -U url-quote-magic
 zle -N self-insert url-quote-magic
 
-## file rename magick
+# file rename magick
 bindkey "^[m" copy-prev-shell-word
 
-## jobs
+# jobs
 setopt long_list_jobs
 
-## pager
+# pager
 export PAGER="less"
 export LESS="-R"
 
